e52045af92d64cf937e8c472774137e7880852f0
Joe Sadoski <jdsadoski@gmail.com>
2020-12-25 02:23:32 -0500

    Establishes web -> discord I/O

--- api.js
+++ api.js
@@ -8,11 +8,18 @@ api.start = (dserver, port) => {
   const app = express();
   app.use(express.json()); // for parsing type=application/json
 
-  const routes = router.setserver(dserver);
+  // serve static directory
+  app.use(express.static("static"));
+
+  // serve admin client on root
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/adminclient/index.html"));
   });
+
+  // init router with discord server reference
+  const routes = router.setserver(dserver);
   app.use("/api", routes);
+
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
   });
