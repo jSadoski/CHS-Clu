cdddf934dcf4c877e34d0531aa9b7ea6bc86aef6
Joe Sadoski <jdsadoski@gmail.com>
2020-12-29 23:19:22 -0500

    Changes exposed API port

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const client = require("./discord/client");
 // const Sequelize = require("sequelize");
 const api = require("./server/server");
 
-if (("BOT_TOKEN", "PORT", "SERVER" in process.env)) {
+if (("BOT_TOKEN", "DISCORD_SERVER" in process.env)) {
   client
     .start(process.env.BOT_TOKEN)
     .then((client) =>
       api.start(
         client.guilds.cache
           .array()
-          .filter((guild) => guild.id == process.env.SERVER)[0],
-        process.env.PORT
+          .filter((guild) => guild.id == process.env.DISCORD_SERVER)[0],
+        80
       )
     )
     .catch((err) => console.log(err.message));
 } else {
   console.log(
-    `Missing required environment variables.\nBOT_TOKEN: ${process.env.BOT_TOKEN}, PORT: ${process.env.PORT}, SERVER: ${process.env.SERVER}`
+    `Missing required environment variables.\nBOT_TOKEN: ${process.env.BOT_TOKEN}, DISCORD_SERVER: ${process.env.DISCORD_SERVER}`
   );
 }
