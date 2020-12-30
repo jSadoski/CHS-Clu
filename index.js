0f3da1e5be98e90bc8cc48749d693a7e8090c618
Joe Sadoski <jdsadoski@gmail.com>
2020-12-29 23:35:40 -0500

    Re-adds port to express server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const client = require("./discord/client");
 // const Sequelize = require("sequelize");
 const api = require("./server/server");
 
-if (("BOT_TOKEN", "DISCORD_SERVER" in process.env)) {
+if (("BOT_TOKEN", "PORT", "DISCORD_SERVER" in process.env)) {
   client
     .start(process.env.BOT_TOKEN)
     .then((client) =>
@@ -10,12 +10,12 @@ if (("BOT_TOKEN", "DISCORD_SERVER" in process.env)) {
         client.guilds.cache
           .array()
           .filter((guild) => guild.id == process.env.DISCORD_SERVER)[0],
-        80
+        process.env.PORT
       )
     )
     .catch((err) => console.log(err.message));
 } else {
   console.log(
-    `Missing required environment variables.\nBOT_TOKEN: ${process.env.BOT_TOKEN}, DISCORD_SERVER: ${process.env.DISCORD_SERVER}`
+    `Missing required environment variables.\nBOT_TOKEN: ${process.env.BOT_TOKEN}, PORT: ${process.env.PORT},DISCORD_SERVER: ${process.env.DISCORD_SERVER}`
   );
 }
