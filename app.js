96bfa0b48de23d10219edc9be95b7a37e7fa9653
Joe Sadoski <jdsadoski@gmail.com>
2021-01-13 02:54:42 -0500

    Structures return object and provides a destructor

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const api = require("./server/server");
 module.exports = async (token, serverID, port) => {
   const instance = {};
   instance.client = await client.start(token);
-  instance.api = await api.start(
-    client.guilds.cache.array().filter((guild) => guild.id == serverID)[0],
-    port
-  );
+  instance.guild = instance.client.guilds.cache
+    .array()
+    .filter((guild) => guild.id == serverID)[0];
+  instance.http = await api(instance.guild, port);
+
+  instance.close = (done) => {
+    Promise.all([instance.client.destroy(), instance.http.close()])
+      .then(done)
+      .catch((err) => console.log(err));
+  };
+
   return instance;
 };
