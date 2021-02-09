bbf34df7b02c0460ee91445644ce01254bf0c860
Joe Sadoski <jdsadoski@gmail.com>
2021-02-09 03:10:34 -0500

    Throws an error if env vars are missing

--- tests/endpoint.test.js
+++ tests/endpoint.test.js
@@ -14,7 +14,7 @@ describe("Server Endpoint", () => {
       );
       http = instance.http;
     } else {
-      console.log(
+      throw new Error(
         `Missing required environment variables.\nBOT_TOKEN: ${process.env.BOT_TOKEN}, PORT: ${process.env.PORT},DISCORD_SERVER: ${process.env.DISCORD_SERVER}`
       );
     }
